b1afe6f2787807501473cae504368f6a111480ea
Lucas Lei <huipeng.lei@mail.utoronto.ca>
2022-11-25 17:21:47 -0500

    ...

--- tagger.py
+++ tagger.py
@@ -4,10 +4,72 @@
 import os
 import sys
 
+END_SENTENCE_SIG = [".", "?", "!"]
+ALL_TAGS = ['AJ0', 'AJC', 'AJS', 'AT0', 'AV0', 'AVP', 'AVQ', 'CJC', 'CJS', 'CJT', 'CRD', 'DPS', 'DT0', 'DTQ', 'EX0', 'ITJ', 'NN0', 'NN1', 'NN2', 'NP0', 'ORD', 'PNI', 'PNP', 'PNQ', 'PNX', 'POS', 'PRF', 'PRP', 'PUL', 'PUN', 'PUQ', 'PUR', 'TO0', 'UNC', 'VBB', 'VBD', 'VBG', 'VBI', 'VBN', 'VBZ', 'VDB', 'VDD', 'VDG', 'VDI', 'VDN', 'VDZ', 'VHB', 'VHD', 'VHG', 'VHI', 'VHN', 'VHZ', 'VM0', 'VVB', 'VVD', 'VVG', 'VVI', 'VVN', 'VVZ', 'XX0', 'ZZ0', 'AJ0-AV0', 'AJ0-VVN', 'AJ0-VVD', 'AJ0-NN1', 'AJ0-VVG', 'AVP-PRP', 'AVQ-CJS', 'CJS-PRP', 'CJT-DT0', 'CRD-PNI', 'NN1-NP0', 'NN1-VVB', 'NN1-VVG', 'NN2-VVZ', 'VVD-VVN']
+# print(len(ALL_TAGS))
+AMBIGUITY_TAGS = ['AJ0-AV0', 'AJ0-VVN', 'AJ0-VVD', 'AJ0-NN1', 'AJ0-VVG', 'AVP-PRP', 'AVQ-CJS', 'CJS-PRP', 'CJT-DT0', 'CRD-PNI', 'NN1-NP0', 'NN1-VVB', 'NN1-VVG', 'NN2-VVZ', 'VVD-VVN']
+
 
 def read_file_to_list(filename):
     f = open(filename, 'r')
     return f.readlines()
+
+
+def _ambiguity_check(tag):
+    """Check the ambiguity tag, if it's not in the desired order, replace
+    For example: 'AJ0-AV0' == 'AV0-AJ0'
+        If we encountered 'AV0-AJ0', it's in the AMBIGUITY_TAGS, we just switch
+            AV0-AJ0 to AJ0-AV0
+    """
+    if len(tag) > 3 and tag not in AMBIGUITY_TAGS:
+        split = tag.split("-")
+        tag = str(split[1] + "-" + split[0])
+        return tag
+    return tag
+
+
+def _initial_probability(word_tag):
+    """Compute the probability of a tag being the first in a sentence.
+    Initial Matrix: A dictionary with tag as key and probalility of first tag
+                        as val
+    Transition Matrix: A dictionary ...
+        key: query (tag after)
+        val = dict...
+                key = evidence (tag before)
+                val = probability of (query | evidence)
+    >>> wt = []
+    >>> _initial_probability(wt)
+    """
+    tag_first_dict = dict.fromkeys(ALL_TAGS, 0)
+    tag_count_dict = dict.fromkeys(ALL_TAGS, 0)
+    # init the transition matrix
+    transition_dict = dict.fromkeys(ALL_TAGS, dict.fromkeys(ALL_TAGS, 0))
+
+    # print(tag_first_dict)
+    word, tag = word_tag[0]
+    tag_first_dict[tag] += 1
+    tag_count_dict[tag] += 1
+    for i in range(1, len(word_tag)):
+        prev_word, prev_tag = word_tag[i - 1][0], word_tag[i - 1][1]
+        word, tag = word_tag[i][0], word_tag[i][1]
+        prev_tag = _ambiguity_check(prev_tag)
+        tag = _ambiguity_check(tag)
+
+        transition_dict[tag][prev_tag] += 1     # tag comes after prev_tag
+        if prev_word in END_SENTENCE_SIG:
+            # tag is at the beginning of the sentence
+            tag_first_dict[tag] += 1
+        tag_count_dict[tag] += 1
+
+    ## Initial Probability
+    # Compute the probability of each tag starts at a sentence
+    for k in tag_first_dict:
+        if tag_count_dict[k] != 0:
+            tag_first_dict[k] /= tag_count_dict[k]
+    print(tag_first_dict)
+
+    ## Compute the transition probability
+
 
 
 def tag(training_list, test_file, output_file):
@@ -21,7 +83,13 @@ def tag(training_list, test_file, output_file):
     for train_file in training_list:
         full_training_list.extend(read_file_to_list(train_file))
     print(len(full_training_list))
-
+    for i in range(len(full_training_list)):
+        full_training_list[i] = full_training_list[i].replace(" ", "").strip().split(':')
+        if len(full_training_list[i]) > 2 and full_training_list[i][-1] == "PUN":
+            full_training_list[i] = [':', 'PUN']
+    print(full_training_list[0:13])
+    # Find the initial probability matrix
+    _initial_probability(full_training_list)
 
 
 if __name__ == '__main__':
---
+++ utils.py
@@ -0,0 +1,29 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from bs4 import BeautifulSoup
+
+
+URL = "https://q.utoronto.ca/courses/278996/pages/part-of-speech-tags-for-assignment-4"
+
+driver = webdriver.Chrome()
+driver.get(URL)
+time.sleep(5)
+username = driver.find_element(By.ID, "username")
+password = driver.find_element(By.ID, "password")
+
+username.send_keys('leihuipe')
+password.send_keys('Shenfeng1234')
+login = driver.find_element(By.CLASS_NAME, "btn-lg")
+login.click()
+
+time.sleep(5)
+p_tags = driver.find_elements(By.TAG_NAME, "p")
+text = []
+for i in range(2, 63):
+    p_text = p_tags[i].text.split(" ")[0]
+    text.append(p_text)
+for i in range(64, len(p_tags)):
+    text.extend(p_tags[i].text.split(" "))
+print(text)
